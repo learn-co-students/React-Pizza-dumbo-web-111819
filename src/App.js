3605356e9cdea022a7866201b9cb4d97518623ec
MohammedQasim <mqshaikh8@gmail.com>
2020-01-31 21:08:34 -0500

    first

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+  state ={
+    pizzas:[],
+    selectedPizza: {}
+  }
+
+  selectPizza = (id) => {
+    // console.log(id)
+
+    let thePizzaChosen = {...this.state.pizzas.find(pizza => pizza.id === id)}
+    this.setState({
+      selectedPizza: thePizzaChosen
+    })
+  }
+
+  pizzaUpdater = () => {
+
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pizzas")
+    .then(r => r.json())
+    .then(pizzaArr => {
+      this.setState({
+        pizzas:pizzaArr
+      })
+    })
+    
+  }
+
+    
   render() {
+    // let selectedPizza = () => this.state.pizzas.find(pizza => pizza.id === this.state.selectedPizzaId)
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizza={this.state.selectedPizza} />
+        <PizzaList giftfromPop={this.selectPizza} pizzaArr={this.state.pizzas}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,26 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = ({pizza:{topping,size,vegetarian}}) => {
+
+    let handleSubmit = (evt)=> {
+      console.log(evt)
+    }
+    let myTopping = topping, mySize = size, isVegetarian = vegetarian; 
+    let updater = (evt) => {
+
+      myTopping = evt.target.value
+    }
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
+            <input type="text" className="form-control" onChange={updater}
+            placeholder="Pizza Topping" value={
                 //Pizza Topping Should Go Here
-                null
+                myTopping
               }/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={size} className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +28,31 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Vegetarian" checked={vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={!vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={handleSubmit}>Submit</button>
         </div>
       </div>
 
   )
 }
+
+// PizzaForm.defaultProps = {
+//   topping:"Pizza Topping",
+//   size:"small",
+//   vegetarian: false
+// }
+
 
 export default PizzaForm
